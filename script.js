81cd796e0c281048cd5f18950caf9964163b9ed0
arihantv140305 <arihant.vashista14@gmail.com>
2023-04-19 19:31:36 +0530

    added files

---
+++ script.js
@@ -0,0 +1,6 @@
+/* JavaScript */
+function submitMessage() {
+    const fname = document.getElementById('Name');
+    const email = document.getElementById('Email');
+    alert("Thank you " + fname.value + " (email: " + email.value + ") for your meassage!");
+}
